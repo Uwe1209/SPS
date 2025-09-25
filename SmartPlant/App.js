65fa097860eef98ed95111a6c910e6c210feb9a0
Uwe1209 <liewyouwee123@gmail.com>
2025-09-25 21:04:26 +0800

    added map to project

--- SmartPlant/App.js
+++ SmartPlant/App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import MapPage from './src/MapPage';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Map">
+        <Stack.Screen name="Map" component={MapPage}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
