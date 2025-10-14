206f467f0815ef47d2dd9d53bac78b5069178a59
darklorddad <danieltiong000@gmail.com>
2025-10-14 23:14:28 +0800

    feat: Add UI for model saving and loading

--- backend/gui.py
+++ backend/gui.py
@@ -9,10 +9,24 @@ def main(page: ft.Page):
             data_dir_path.value = e.path
             page.update()
 
+    def on_save_dialog_result(e: ft.FilePickerResultEvent):
+        if e.path:
+            save_model_path.value = e.path
+            page.update()
+
+    def on_load_dialog_result(e: ft.FilePickerResultEvent):
+        if e.files:
+            load_model_path.value = e.files[0].path
+            page.update()
+
     file_picker = ft.FilePicker(on_result=on_dialog_result)
-    page.overlay.append(file_picker)
+    save_file_picker = ft.FilePicker(on_result=on_save_dialog_result)
+    load_file_picker = ft.FilePicker(on_result=on_load_dialog_result)
+    page.overlay.extend([file_picker, save_file_picker, load_file_picker])
 
     data_dir_path = ft.TextField(label="Dataset Directory", read_only=True, expand=True)
+    save_model_path = ft.TextField(label="Save Model Path", read_only=True, expand=True)
+    load_model_path = ft.TextField(label="Load Model Path", read_only=True, expand=True)
 
     page.add(
         ft.Dropdown(
@@ -33,6 +47,28 @@ def main(page: ft.Page):
                     ),
                 ),
                 data_dir_path,
+            ]
+        ),
+        ft.Row(
+            [
+                ft.ElevatedButton(
+                    "Save Model As...",
+                    on_click=lambda _: save_file_picker.save_file(
+                        dialog_title="Save Model As..."
+                    ),
+                ),
+                save_model_path,
+            ]
+        ),
+        ft.Row(
+            [
+                ft.ElevatedButton(
+                    "Load Model From...",
+                    on_click=lambda _: load_file_picker.pick_files(
+                        dialog_title="Load Model From...", allow_multiple=False
+                    ),
+                ),
+                load_model_path,
             ]
         ),
         ft.TextField(label="Number of Epochs"),
